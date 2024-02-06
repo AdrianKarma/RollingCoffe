b2b9ae4e9c13a709f7d8fa40df8a674762ce4f4f
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-02-06 17:42:26 -0300

    agrego rutas

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,26 @@ import Administrador from './components/pages/Administrador'
 import Inicio from './components/pages/Inicio'
 import Error404 from './components/pages/Error404';
 import "bootstrap-icons/font/bootstrap-icons.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <>
-    <section className='principal'>
-    <article className='nuevoMain'>
-      <NavCoffee></NavCoffee>
-   {/*<Inicio></Inicio>*/}
-    {/* <Error404></Error404>*/}
-   <Administrador></Administrador>
-  </article>
-    <FootCoffe></FootCoffe>
-    </section>
+   
+    <BrowserRouter>
+    <NavCoffee></NavCoffee>
+    <Routes >
+    <Route exact path='/' element={<Inicio></Inicio>}></Route>
+    <Route exact path='administrador' element={<Administrador></Administrador>}></Route>
+   
+    <Route exact path='*' element={<Error404></Error404>}></Route>
 
 
-    </>
+
+    </Routes>
+<FootCoffe></FootCoffe>
+    </BrowserRouter>
     
   )
 }
--- src/components/common/NavCoffee.jsx
+++ src/components/common/NavCoffee.jsx
@@ -2,21 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/Coffee_Logo.png'
+import { Link, NavLink,   } from 'react-router-dom';
 
 const NavCoffee = () => {
     return (
         <>
          <Navbar expand="lg" className=" bg-gray ">
       <Container>
-        <Navbar.Brand href="#home" > <img src={logo} alt="" className='img-fluid ' width={200}/></Navbar.Brand>
+        <Navbar.Brand as={Link} to={'/'}> <img src={logo} alt="" className='img-fluid ' width={200}/></Navbar.Brand>
        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Administrador</Nav.Link>
-            <Nav.Link href="#home">Registro</Nav.Link>
-            <Nav.Link href="#home">Login</Nav.Link>
+            <NavLink className='nav-link' to={'/'} end>Inicio</NavLink>
+            <NavLink className='nav-link' to={'administrador'} end>Administrador</NavLink>
+            <NavLink className='nav-link' to={'login'} end>Registro</NavLink>
+            <NavLink className='nav-link' to={'registro'} end>Login</NavLink>
             
           </Nav>
         </Navbar.Collapse>
