303dc6f015c802869288a4794b6085d42f3f9adf
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-02-08 16:50:59 -0300

    agregamos un producto en el db.json

--- src/components/common/FormularioProducto.jsx
+++ src/components/common/FormularioProducto.jsx
@@ -8,19 +8,19 @@ const FormularioProducto = () => {
         <h1>Nuevo Producto</h1>
         <hr />
         <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3" controlId="Producto.form">
             <Form.Label>Producto*</Form.Label>
             <Form.Control type="text" placeholder="Ej: café" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3" controlId="Precio.form">
             <Form.Label>Precio*</Form.Label>
             <Form.Control type="text" placeholder="ej: 1300" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3" controlId="URL.form">
             <Form.Label>Imagen URL*</Form.Label>
             <Form.Control type="text" placeholder="https://www.google.com" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3" controlId="Categoria.form">
             <Form.Label>Categoria*</Form.Label>
             <Form.Select type="text" aria-label="Elige una Categoria">
               <option>Open this select menu</option>
@@ -31,12 +31,12 @@ const FormularioProducto = () => {
             </Form.Select>
             
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Group className="mb-3" controlId="breve.form">
             <Form.Label>Descripción breve*</Form.Label>
             <Form.Control as="textarea" rows={2} />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Descripción Amplia*</Form.Label>
+          <Form.Group className="mb-3" controlId="amplia.form">
+            <Form.Label>Descripción amplia*</Form.Label>
             <Form.Control as="textarea" rows={3} />
           </Form.Group>
           <Button variant="success">Guardar</Button>
