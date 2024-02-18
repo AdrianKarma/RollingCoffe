71025597070d401938d98d16b68312150fa6487c
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-02-18 17:57:56 -0300

    modifico el db.json

--- src/components/common/FormularioProducto.jsx
+++ src/components/common/FormularioProducto.jsx
@@ -1,16 +1,40 @@
 import { Container, Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form"
+
 
 const FormularioProducto = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const productoValidado = (producto) => {
+    console.log(producto);
+  };
+
   return (
     <>
       <Container className="my-4">
         <h1>Nuevo Producto</h1>
         <hr />
-        <Form>
+        <Form onSubmit={handleSubmit(productoValidado)}>
           <Form.Group className="mb-3" controlId="Producto.form">
             <Form.Label>Producto*</Form.Label>
-            <Form.Control type="text" placeholder="Ej: café" />
+            <Form.Control type="text" placeholder="Ej: café" {
+                ...register("nombreProducto", {
+                    required:"El nombre del producto es obligatorio",
+                     minLenght:{
+                    value:2,
+                    message:"debe ingresar como minimo 2 caracteres para el nombre del producto"
+                }, 
+                    maxLength:{
+                    value:50,
+                     message:"debe ingresar como maximo 50 caracteres para el nombre del producto"
+                }})
+            } />
+            <Form.Text className="text-danger">{errors.nombreProducto?.message} </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="Precio.form">
             <Form.Label>Precio*</Form.Label>
@@ -27,9 +51,7 @@ const FormularioProducto = () => {
               <option value="1">One</option>
               <option value="2">Two</option>
               <option value="3">Three</option>
-              
             </Form.Select>
-            
           </Form.Group>
           <Form.Group className="mb-3" controlId="breve.form">
             <Form.Label>Descripción breve*</Form.Label>
